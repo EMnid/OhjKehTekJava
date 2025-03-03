b27f81b3652e085177f8596422083b2b8ae979b7
Eetu-Matti Mäkinen <eetu-matti.makinen@tuni.fi>
2025-03-03 11:39:06 +0200

    Worked on wk6 Today-class

--- src/wk6/Today.java
+++ src/wk6/Today.java
@@ -1,42 +1,54 @@
 package tamk.ohsyte;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.MonthDay;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Today {
     public static void main(String[] args) {
         // Gets the singleton manager. Later calls to getInstance
         // will return the same reference.
-        EventManager manager = EventManager.getInstance();
+        tamk.ohsyte.EventManager manager = tamk.ohsyte.EventManager.getInstance();
 
         // Add a couple of event providers for testing purposes
-        manager.addEventProvider(new FirstEventProvider("first"));
-        manager.addEventProvider(new SecondEventProvider("second"));
+        manager.addEventProvider(new tamk.ohsyte.FirstEventProvider("first"));
+        manager.addEventProvider(new tamk.ohsyte.SecondEventProvider("second"));
 
         // Add a CSV event provider that reads from the given file.
         // Replace with a valid path to the events.csv file on your own computer!
         final String fileName = "C:/Users/eetu-/today/events.csv";
-        manager.addEventProvider(new CSVEventProvider(fileName));
+        tamk.ohsyte.CSVEventProvider csvProvider = new tamk.ohsyte.CSVEventProvider(fileName);
+        boolean added = manager.addEventProvider(csvProvider);
+        System.out.println("CSVEventProvider added: " + added);
 
-        List<Event> events = manager.getAllEvents();
+        // Try to add the same CSVEventProvider again
+        added = manager.addEventProvider(csvProvider);
+        System.out.println("CSVEventProvider added again: " + added);
 
+        // Remove the CSVEventProvider
+        boolean removed = manager.removeEventProvider(csvProvider.getIdentifier());
+        System.out.println("CSVEventProvider removed: " + removed);
+
+        // Verify removal by trying to remove it again
+        removed = manager.removeEventProvider(csvProvider.getIdentifier());
+        System.out.println("CSVEventProvider removed again: " + removed);
+
+        // Print the final state of the manager
+        List<tamk.ohsyte.Event> events = manager.getAllEvents();
         int providerCount = manager.getEventProviderCount();
         int eventCount = events.size();
         System.out.printf("Manager has %d event providers,%n", providerCount);
         System.out.printf("with a total of %d events.%n", eventCount);
 
-
         List<String> identifiers = manager.getEventProviderIdentifiers();
-        System.out.println("Event providers: "
-                + Arrays.toString(identifiers.toArray()));
+        System.out.println("Event providers: " + Arrays.toString(identifiers.toArray()));
 
-
-        manager.removeEventProvider("CSV");
-
-        providerCount = manager.getEventProviderCount();
-        events = manager.getAllEvents();  // refresh event list
-        eventCount = events.size();
-        System.out.printf("Manager has %d event providers,%n", providerCount);
-        System.out.printf("with a total of %d events.%n", eventCount);
+        // Print the number of events from the CSV file
+        int csvEventCount = csvProvider.getEvents().size();
+        System.out.printf("CSV file contains %d events.%n", csvEventCount);
     }
 }
