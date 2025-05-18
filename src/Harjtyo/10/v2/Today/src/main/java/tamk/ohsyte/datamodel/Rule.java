a75a3b317b4a21c7c74d7ba587256efb1c78c320
Eetu-Matti Mäkinen <eetu-matti.makinen@tuni.fi>
2025-05-18 17:29:20 +0300

    Moved folders

--- src/Harjtyo/10/v2/Today/src/main/java/tamk/ohsyte/datamodel/Rule.java
+++
@@ -1,8 +0,0 @@
-package tamk.ohsyte.datamodel;
-
-import java.time.LocalDate;
-import java.time.MonthDay;
-
-public interface Rule {
-    MonthDay getMonthDay(int year);
-}
--- src/Harjtyo/10/v2/Today/src/main/java/tamk/ohsyte/providers/WritableEventProvider.java
+++
@@ -1,7 +0,0 @@
-package tamk.ohsyte.providers;
-
-import tamk.ohsyte.datamodel.Event;
-
-public interface WritableEventProvider extends EventProvider {
-    boolean addEvent(Event event);
-}
